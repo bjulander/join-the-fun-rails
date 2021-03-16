b8ae09e94fc54e06753d54c2d111d240d22376f0
Blake Julander <13lakejulander@gmail.com>
2021-03-16 09:39:34 -0700

    commit tree bf19b06b453463d913a197b03d4948dd83216903

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -1,7 +1,8 @@
-class CreateTaxis < ActiveRecord::Migration
+class CreateTaxis < ActiveRecord::Migration[4.2]
   def change
     create_table :taxis do |t|
       t.timestamps null: false
+
     end
   end
 end
